7376702eae4db4104f10165ae838afefc8fb40a5
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-08-14 18:23:46 +0200

    Do some experiments with spotify API. Created functions for retrieving tracks metadata and audio features

--- webapp/app/spotify_api/spotify_api.py
+++ webapp/app/spotify_api/spotify_api.py
@@ -4,8 +4,11 @@ class SpotifyApi:
     def __init__(self, access_token):
         self._access_token = access_token
         self._headers = {'Authorization': 'Bearer {}'.format(access_token)}
+        self._LONG_LIMIT = 100
+        self._SHORT_LIMIT = 50
 
     def GetPlaylistInfo(self, id: str) -> dict:
+        #delete it later -?
         r = requests.get(f"https://api.spotify.com/v1/playlists/{id}", headers=self._headers)
         return r.json()
     
@@ -26,28 +29,75 @@ class SpotifyApi:
 
         return l
     
+    def RequestPlaylist(self, id: str) -> dict:
+        r = requests.get(f"https://api.spotify.com/v1/playlists/{id}", headers=self._headers)
+        return r.json()
+    
     def requestSongsAudioFeatures(self, songs_ids: list) -> list[dict]:
-        pass
-
-    def requestSongsMetadata(self, songs_ids) -> list[dict]:
-        if type(songs_ids) == str:
-            return self.requestSongsMetadataByPlaylist(songs_ids)
-        elif type(songs_ids) == list:
-            return self.requestSongsMetadataByList(songs_ids)
-        else:
-            return False
+        offset = 0
+        json_response = {}
+        while len(songs_ids) > offset:
+            r = requests.get(
+                f"https://api.spotify.com/v1/audio-features?ids={','.join(songs_ids[offset:self._LONG_LIMIT+offset])}", 
+                headers=self._headers)
+            offset += self._LONG_LIMIT
+            json_response += r.json()
         
-    def requestSongsMetadataByPlaylist(self, playlist_id: str) -> list[dict]:
-        pass
+        return json_response
+
+    # def requestSongsMetadata(self, songs_ids) -> list[dict]:
+    #     if type(songs_ids) == str:
+    #         return self.requestSongsMetadataByPlaylist(songs_ids)
+    #     elif type(songs_ids) == list:
+    #         return self.requestSongsMetadataByList(songs_ids)
+    #     else:
+    #         return False
+        
+    # def requestSongsMetadataByPlaylist(self, playlist_id: str) -> list[dict]:
+    #     r = requests.get(f"https://api.spotify.com/v1/playlists/{id}", headers=self._headers)
+    #     return r.json()
 
     def requestSongsMetadataByList(self, songs_ids: list) -> list[dict]:
-        pass
+        offset = 0
+        audio_features = []
+        while len(songs_ids) > offset:
+            r = requests.get(
+                f"https://api.spotify.com/v1/audio-features?ids={','.join(songs_ids[offset:self._SHORT_LIMIT+offset])}", 
+                headers=self._headers)
+            offset += self._SHORT_LIMIT
+            audio_features += r.json()["audio_features"]
+        
+        return audio_features
     
-    def retrieveSongsAudioFeatures(self, songs: list[dict]) -> list[dict]:
-        pass
+    def retrieveIdsFromPlaylist(self, playlist: dict) -> list:
+        track_ids = []
+        for t in playlist["tracks"]["items"]:
+            track_ids.append(t["track"]["id"])
+        return track_ids
+    
+    def retrieveSongsAudioFeatures(self, songs_ids: list) -> list[dict]:
+        offset = 0
+        audio_features = []
+        while len(songs_ids) > offset:
+            r = requests.get(
+                f"https://api.spotify.com/v1/audio-features?ids={','.join(songs_ids[offset:self._SHORT_LIMIT+offset])}", 
+                headers=self._headers)
+            offset += self._SHORT_LIMIT
+            audio_features += r.json()["audio_features"]
+        
+        return audio_features
 
-    def retrieveSongsMetadata(self, songs: list[dict]) -> list[dict]:
-        pass
+    def retrieveSongsMetadata(self, songs_ids: list) -> list[dict]:
+        offset = 0
+        audio_features = []
+        while len(songs_ids) > offset:
+            r = requests.get(
+                f"https://api.spotify.com/v1/tracks?ids={','.join(songs_ids[offset:self._SHORT_LIMIT+offset])}", 
+                headers=self._headers)
+            offset += self._SHORT_LIMIT
+            audio_features += r.json()["tracks"]
+        
+        return audio_features
     
     def RetrievePlaylistMetadata(self, playlist: dict) -> dict:
 
