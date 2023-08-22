526982b57f48a8aefffbbb2ee892ceae2712443f
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-08-22 09:57:31 +0200

    Experimenting with combining plotly and flask

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,10 +1,13 @@
 from . import app
 from flask import render_template, redirect
 from app.forms import SpotifyURL
+import plotly.express as px
+import plotly
+import json
 
 from .spotify_api.spotify_connection import SpotifyConn
 from .spotify_api.spotify_api import SpotifyApi
-from .spotify_api.spotify_utils import load_credentials
+from .spotify_api.spotify_utils import load_credentials, load_playlist_info
 
 
 # main = Blueprint('main', __name__) only for blueprints
@@ -16,17 +19,22 @@ def index():
 
 @app.route("/index_post", methods=["POST"])
 def index_post():
+    #TODO load id from form
     token = get_spotify_access_token()
     api = SpotifyApi(token)
 
-    p = api.RequestPlaylist("71mFAErIZDf0h18roaz9oq")  # 387OhCc6mEbm96wzfFfhpp
-    print(len(api.retrieveIdsFromPlaylist(p)))
+    p = api.RequestPlaylist("3vFJuKtDQCYkFSrpUaf5Dh")  # 387OhCc6mEbm96wzfFfhpp
+    playlist_meta = api.RetrievePlaylistMetadata(p)
 
-    # p = api.GetPlaylistInfo("387OhCc6mEbm96wzfFfhpp")
-    d = api.RetrievePlaylistMetadata(p)
+    ids = api.retrieveIdsFromPlaylist("3vFJuKtDQCYkFSrpUaf5Dh", playlist_meta["total_count"])
 
-    print(d)
-    return render_template('analysis.html')
+    tracks_info = load_playlist_info(api, ids)
+
+    fig = px.scatter(tracks_info, x="valence", y="danceability", size = "popularity", hover_data=['artist_name', 'name'])
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    return render_template('analysis.html', playlist_info = playlist_meta, tracks_info = tracks_info, graph=graphJSON)
 
 @app.route("/analysis")
 def analysis():
