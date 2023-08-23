61781e5eeca1276e9ca9fb0b6a069c53e908bad2
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-08-23 13:24:13 +0200

    Add placeholder value to input for playlist's url/id

--- webapp/app/forms.py
+++ webapp/app/forms.py
@@ -3,4 +3,4 @@ from wtforms import StringField, URLField
 from wtforms.validators import DataRequired, URL
 
 class SpotifyURL(FlaskForm):
-    url = StringField('URL', validators=[DataRequired(), URL(False)])
+    url = StringField('URL', validators=[DataRequired(), URL(False)], render_kw={"placeholder": "URL or ID of Spotify playlist."})
