9fc3850c91421479f1f88a53f9a66e0f3b7d4d94
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-08-23 10:35:57 +0200

    More data is retrieved for visualization of every audio parameter

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -62,33 +62,42 @@ def analysis():
 
 def getSpotifyStatistic(df, stat_name):
 
-    top = getTopNumerical(df, stat_name, 3)
-
-    top_val = top.values()
-    top_index = top.keys()
+    top_artists, top_titles, top_vals = getTopNumerical(df, stat_name, 3)
+    bot_artists, bot_titles, bot_vals = getTopNumerical(df, stat_name, 3, get_bottom=True)
 
     fig = px.histogram(df, x=stat_name, nbins=10, marginal="violin")
     figJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     d={
         "name": stat_name,
-        "top_values": top_val,
-        "top_index": top_index,
+        "top_artists": top_artists,
+        "top_values": top_vals,
+        "top_titles": top_titles,
+        "bot_artists": bot_artists,
+        "bot_values": bot_vals,
+        "bot_titles": bot_titles,
         "plot": figJSON
     }
     return d
 
-def getTopNumerical(df, column, topn, add_exaquo = False):
+def getTopNumerical(df, column, topn, add_exaquo = False, get_bottom = False):
 
-    keys = list(df.sort_values(by=column, ascending = False).head(topn)["name"])
-    values = list(df[column].sort_values(ascending = False).head(topn).values)
+    extracted_data = df.sort_values(by=column, ascending = get_bottom).head(topn)[["name", "artist_name", column]]
+    values = extracted_data[column].to_list()
+    artists = extracted_data["artist_name"].to_list()
+    titles = extracted_data["name"].to_list()
+    print(artists)
+    print(titles)
+    print(values)
+
+    
 
     if add_exaquo:
         s = df[column].value_counts()[(df[column].value_counts() == values[-1]) & (~df[column].value_counts().index.isin(keys))]
         values += (list(s.values))
         keys += (list(s.index))
 
-    return dict(zip(keys, values))
+    return artists, titles, values
 
 
 def getTop(df, column, topn, add_exaquo = False):
