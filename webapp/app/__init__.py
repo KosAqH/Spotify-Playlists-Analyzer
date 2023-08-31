f5b636f2ba817797775658b18eb5559c6b997808
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-08-31 14:23:37 +0200

    Fix displaying basic info about playlists on comparison page

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__, instance_relative_config=False)
 
-# app.config['SECRET_KEY'] = 'this-shouldnt-be-here-in-real-app'
+app.config['SECRET_KEY'] = 'this-shouldnt-be-here-in-real-app'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' only for db
 
 from . import views
