0c787cd44bfed7ba6a73491e5f9e25d36d783226
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-09-01 10:57:18 +0200

    Add docstrings

--- webapp/app/spotify_api/audio_data.py
+++ webapp/app/spotify_api/audio_data.py
@@ -1,11 +1,26 @@
 import pandas as pd
 
 class AudioData:
+    """
+    Class containing some utilities function to transform data requested with API.
+    """
     def __init__(self):
+        """
+        Constructor of class do nothing
+        """
         pass
 
     def mergeTracksData(self, df_metadata: pd.DataFrame, df_features: pd.DataFrame) -> pd.DataFrame:
-        # new_df = df_metadata.merge(df_features, on="id")
+        """
+        Function merges dataframes with metadata and features.
+
+            Args:
+                df_metadata (pd.DataFrame) - 
+                df_features (pd.DataFrame) -
+
+            Returns:
+                new_df (pd.DataFrame) - 
+        """
         cols_to_use = df_features.columns.difference(df_metadata.columns) #avoiding duplicates
         new_df = df_metadata.merge(df_features[cols_to_use], 
                                    left_index=True, 
@@ -15,6 +30,18 @@ class AudioData:
         return new_df
     
     def GetTotalDuration(self, df: pd.DataFrame, unit = "second") -> float:
+        """
+        Function calculates total duration of playlist and converts it to chosen unit.
+
+            Args:
+                df (pd.DataFrame) - dataframe containing data regarding tracks. It has to have
+                    'duration_ms' column.
+                unit (str) - one of the values: 'second', 's', 'milisecond', 'ms', 'minute', 'min'
+                    'hour', 'h'
+
+            Returns:
+                duration (float) - total duration of playlist in chosen unit  
+        """
         duration = df["duration_ms"].sum() 
 
         if unit == "second" or unit == "s":
