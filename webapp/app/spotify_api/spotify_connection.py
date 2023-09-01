2793c9d72a829216f1c64bbeaa6f2d60fa1b4f20
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-09-01 11:08:13 +0200

    Add docstrings and refactor

--- webapp/app/spotify_api/spotify_connection.py
+++ webapp/app/spotify_api/spotify_connection.py
@@ -1,13 +1,25 @@
-import os
-from dotenv import load_dotenv
 import requests
 
 class SpotifyConn:
+    """
+    Class used for connecting with API and obtaining access token.
+    """
     def __init__(self):
+        """
+        Constructor sets variables of necessary URL's.
+        """
         self.auth_url = 'https://accounts.spotify.com/api/token'
         self.api_url = "https://api.spotify.com/v1/"
 
     def authorize(self, client_id: str, api_secret: str) -> None:
+        """
+        Function sends request to obtain access token. Token is saved as 
+            a private attribute of class. None value is returned.
+
+            Args:
+                client_id (str) - client id, loaded from env variables
+                api_secrets (str) - api key, loaded from env variables
+        """
         data = {
             'grant_type': 'client_credentials',
             'client_id': client_id,
@@ -18,6 +30,12 @@ class SpotifyConn:
         self._access_token = self._auth_response.json().get('access_token')
 
     def get_access_token(self) -> str:
+        """
+        Getter function that returns access token.
+
+            Returns:
+                _access_token (str) - acces token that has to be added to every API request
+        """
         return self._access_token
 
 if __name__ == "__main__":
