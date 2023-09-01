24931694959ce52a77792c3bdb9ef6bbc50f2f67
KosAqH <marcin_wlodarczyk@protonmail.com>
2023-09-01 10:33:06 +0200

    Add docstrings and refactor

--- webapp/app/spotify_api/spotify_utils.py
+++ webapp/app/spotify_api/spotify_utils.py
@@ -4,13 +4,34 @@ import pandas as pd
 from app.spotify_api.audio_data import AudioData
 from app.spotify_api.spotify_api import SpotifyApi
 
-def load_credentials():
+def load_credentials() -> [str, str]:
+    """
+    Function loads client id and api key from .env file. More info about this method of
+        authentication may be found here: 
+        https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
+
+        Returns:
+            client_id (str)
+            secret_key (str)
+    """
     load_dotenv()
     client_id = os.getenv("CLIENT_ID")
     secret_key = os.getenv("API_SECRET")
     return client_id, secret_key
 
-def load_playlist_info(api: object, ids: list) -> pd.DataFrame:
+def load_playlist_info(api: SpotifyApi, ids: list) -> pd.DataFrame:
+    """
+    Function returns Dataframe object that contains both audio features and metadata
+        requested from Spotify's API. 
+
+        Args:
+            api (SpotifyApi object): initialized with access token object of class Spotify Api
+            ids (list[str]): list of ids of all songs we want to get data about
+
+        Returns:
+            df (DataFrame object): df containing metadata and audio features data of all songs
+                that id was passed to the function
+    """
     meta = api.retrieveSongsMetadata(ids)
     features = api.retrieveSongsAudioFeatures(ids)
 
@@ -20,9 +41,6 @@ def load_playlist_info(api: object, ids: list) -> pd.DataFrame:
     AD = AudioData()
     df = AD.mergeTracksData(df_meta, df_af)
     return df
-
-def extract_id():
-    pass
 
 if __name__ == "__main__":
     pass
